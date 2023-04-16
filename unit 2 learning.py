4d49467387cc758e96b5a441af654414ed72ec1b
Irina Zorina <irenfeliz@gmail.com>
2023-04-16 18:52:15 +0200

    unit 2

---
+++ unit 2 learning.py
@@ -0,0 +1,21 @@
+# import math
+
+# def print_volume(r):
+#     volume = (4/3) * math.pi * (r ** 3)
+#     print("The volume of a sphere with radius", r, "is", volume)
+
+pi = 3.1415926535897932
+
+
+def print_volume(r):
+    volume = (4 / 3) * pi * (r ** 3)
+    print("The volume of a sphere with radius", r, "is", volume)
+
+print_volume(30)
+# Output: The volume of a sphere with radius 30 is 113097.33552923254
+
+print_volume(75)
+# Output: The volume of a sphere with radius 75 is 1767145.8676442585
+
+print_volume(44)
+# Output: The volume of a sphere with radius 44 is 356817.9048045239
