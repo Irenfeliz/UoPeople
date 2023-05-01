47c1d1ea1280d38eaba3b542682ef72a9118624e
Irenfeliz <irenfeliz@gmail.com>
2023-05-01 18:09:04 +0300

    unit3

---
+++ main.py
@@ -0,0 +1,17 @@
+def counting_sheeps(n):
+    counting_sheeps(n + 1)
+
+counting_sheeps(1)
+
+
+# def counting_sheeps(n):
+#     if n <= 15:
+#         print(n, "sheep")
+#         counting_sheeps(n + 1)
+#     else:
+#         print("Time to sleep!")
+#
+#
+# counting_sheeps(1)
+
+
---
+++ unit3.py
@@ -0,0 +1,26 @@
+def countdown(n):
+    if n <= 0:
+        print('Blastoff!')
+    else:
+        print(n)
+        countdown(n - 1)
+
+
+def countup(n):
+    if n >= 0:
+        print('Blastoff!')
+    else:
+        print(n)
+        countup(n + 1)
+
+
+num = int(input()) # requesting an integer number
+
+if num > 0:
+    countdown(num)  # when number is positive we count down
+else:
+    countup(num)  # when number is negative or zero we count up.
+    # In case of zero, it doesn't matter which function we call,
+    # because zero triggers Blastoff.
+
+
